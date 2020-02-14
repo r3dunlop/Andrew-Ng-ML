8385271daababfb8e8663f3d0db58eb659ca8b7c
Ryan Dunlop <ryan_dunlop@cooperators.ca>
2020-02-14 11:43:32 -0500

    Trained one v all

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -49,12 +49,16 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Set initial theta
+initial_theta = zeros(n+1,1);
 
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
+for i = 1 : num_labels
+  all_theta(i, :) = fmincg (@(t)(lrCostFunction(t, X, (y==i), lambda)),initial_theta,options);
 
-
-
-
+end
 
 
 
