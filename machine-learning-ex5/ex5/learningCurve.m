710e892d52a97adc3e0ebb9d5ae84e763bf0037d
Ryan Dunlop <ryan_dunlop@cooperators.ca>
2020-02-22 11:31:34 -0500

    Added learning curve

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,7 +53,11 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
+for i = 1:m
+  tmp_theta = trainLinearReg(X(1:i,:),y(1:i,:),lambda);
+  error_train(i) = linearRegCostFunction(X(1:i,:),y(1:i,:),tmp_theta,0);
+  error_val(i) = linearRegCostFunction(Xval,yval,tmp_theta,0);
+endfor
 
 
 
