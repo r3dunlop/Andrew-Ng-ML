c15aeaf225f9d1d1b5369bcfb85bd3ea27200d09
Ryan Dunlop <ryan_dunlop@cooperators.ca>
2020-02-26 17:51:34 -0500

    Tuned hyperparameters

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,9 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values_to_try = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+best_C = 0.01;
+best_sigma = 0.01;
+best_error = Inf;
 
+for C = values_to_try
+  for sigma = values_to_try
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    prediction_error = mean(double(predictions ~= yval));
+    
+    if prediction_error < best_error
+      best_C = C;
+      best_sigma = sigma;
+      best_error = prediction_error;
+    endif
+  endfor
+endfor
 
+C = best_C
+sigma = best_sigma
 
 
 
